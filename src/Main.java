b0988fac0dcaaa7feb0edf239725aead7a9deb95
sara mohamed <sara.mohamed1097@gmail.com>
2025-07-04 20:06:16 +0300

    multible test cases

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,10 @@ import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
         Product cheese = ProductFactory.createProduct(
-                "Cheese", 50, 5, true, true, 1.5, LocalDate.now().plusDays(5));
+                "Cheese", 50, 10, true, true, 1.5, LocalDate.now().plusDays(5));
 
         Product tv = ProductFactory.createProduct(
-                "TV", 300, 2, true, false, 10.0, null);
+                "TV", 300, 5, true, false, 10.0, null);
 
         Product biscuits = ProductFactory.createProduct(
                 "Biscuit", 25, 10, false, true, null, LocalDate.now().plusDays(2));
@@ -26,17 +26,53 @@ public class Main {
         cart.addItem(Mobile, 3);
         cart.addItem(biscuits, 1);
 
-        Customer customer1 = new Customer("tara", 2000);
-        Cart cart1 = new Cart();
-        cart1.addItem(cheese, 3);
-        cart1.addItem(tv, 1);
-        cart1.addItem(Mobile, 3);
 
 
         try {
+            System.out.println("use case shippable and non , expired and non ");
             Checkout.checkout(cart, customer);
+
         } catch (Exception e) {
             System.out.println("Checkout failed: " + e.getMessage());
+        }
+
+        //empty cart use case
+        Customer user = new Customer("User", 5000);
+        Cart emptyCart = new Cart();
+
+        try {
+            System.out.println("use case empty cart");
+            Checkout.checkout(emptyCart, user);
+
+        } catch (Exception e) {
+            System.out.println("Checkout failed: " + e.getMessage());
+        }
+
+
+        // out of stock use case
+        Customer user2 = new Customer("StockError", 5000);
+        Cart cart2 = new Cart();
+
+        try {
+            System.out.println("use case out of stock");
+            cart2.addItem(cheese, 10);
+            cart2.addItem(tv,1);
+            Checkout.checkout(cart2, user2);
+        } catch (Exception e) {
+            System.out.println("Failed " + e.getMessage());
+        }
+
+        //use case items is not shipabale
+
+        try {
+            System.out.println("use case no shipping notice as no shippable items");
+            Customer digital = new Customer("DigitalUser", 100);
+            Cart cart3 = new Cart();
+            cart.addItem(Mobile, 2); // not shippable
+            Checkout.checkout(cart, digital);
+
+        } catch (Exception e) {
+            System.out.println("Failed " + e.getMessage());
         }
 
 
